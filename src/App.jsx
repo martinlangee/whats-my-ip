1faba3c020bb0c2ac164fa8e4e45600b30fedf8d
lange <martin.lange@csa-germany.de>
2022-02-10 18:12:19 +0100

    basic structure an style

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">What's My IP?</header>
-      <main>
+      <main id="body">
         <div id="infos">Infos about the location</div>
         <div id="map">Open Street Map frame</div>
       </main>
