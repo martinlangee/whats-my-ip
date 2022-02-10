c28c4feac6e9e2b12f759315ac08615ff81d7609
lange <martin.lange@csa-germany.de>
2022-02-10 17:56:15 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">What's My IP?</header>
+      <main>
+        <div id="infos">Infos about the location</div>
+        <div id="map">Open Street Map frame</div>
+      </main>
+    </div>
+  );
+}
+
+export default App;
