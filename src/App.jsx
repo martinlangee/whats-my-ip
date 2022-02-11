4ee74ecf3abd0d754ebf5ad87579d15b6ffc3d57
lange <martin.lange@csa-germany.de>
2022-02-11 15:52:51 +0100

    get additional country information via rest

--- src/App.jsx
+++ src/App.jsx
@@ -6,25 +6,57 @@ import "./styles/App.css";
 function App() {
   const [localIP, setLocalIP] = useState("");
   const [location, setLocation] = useState(null);
+  const [countryName, setCountryName] = useState("");
 
   useEffect(() => {
-    const url = `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_IPIFY_API_KEY}`;
-    Axios.get(url)
+    getData();
+  }, []);
+
+  const getData = async () => {
+    const countryKey = await getGeoData();
+    getCountryData(countryKey);
+  };
+
+  const getGeoData = () => {
+    return new Promise((resolve) => {
+      Axios.get(
+        `https://geo.ipify.org/api/v2/country,city?apiKey=${process.env.REACT_APP_IPIFY_API_KEY}`
+      )
+        .then((resp) => {
+          console.log(resp);
+          setLocalIP(resp.data.ip);
+          setLocation(resp.data.location);
+          resolve(resp.data.location.country);
+        })
+        .catch((e) => console.log(e));
+    });
+  };
+
+  const getCountryData = (countryKey) => {
+    console.log({ countryKey });
+
+    Axios.get(`https://restcountries.com/v3.1/alpha/${countryKey}`)
       .then((resp) => {
         console.log(resp);
-        setLocalIP(resp.data.ip);
-        setLocation(resp.data.location);
+        setCountryName(resp.data[0].name.common);
       })
       .catch((e) => console.log(e));
-  }, []);
+  };
 
   return (
     <div className="App">
       <header className="App-header">What's My IP?</header>
       <main id="body">
         <div id="infos">
-          <p>Infos about the location</p>
+          <p>
+            <b>Infos about the location</b>
+          </p>
           <p>My current IP: {localIP}</p>
+          <p>Country name: {countryName}</p>
+          <br></br>
+          <p>
+            <b>Generic information</b>
+          </p>
           {location
             ? Object.keys(location).map((key, idx) => (
                 <p key={idx}>
@@ -34,7 +66,9 @@ function App() {
             : ""}
         </div>
         <div id="mapCol">
-          <p>Map frame</p>
+          <p>
+            <b>Map </b>
+          </p>
           {location ? (
             <MyMap id="map" lng={location.lng} lat={location.lat} />
           ) : (
